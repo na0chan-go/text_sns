d6ada72953c230d6aaefbf2bcdf87a5411e4cd13
na0chan-go <nn19960218@gmail.com>
2024-06-04 00:40:01 +0900

    feat: FirestoreのRepositoryを作成

---
+++ lib/repository/firestore_repository.dart
@@ -0,0 +1,16 @@
+import 'package:text_sns/infrastructure/firestore/firestore_client.dart';
+import 'package:text_sns/models/result/result.dart';
+import 'package:text_sns/typedefs/firestore_typedef.dart';
+import 'package:text_sns/typedefs/result_typedef.dart';
+
+class FirestoreRepository {
+  FutureResult<Doc> getDoc(DocRef ref) async {
+    final client = FirestoreClient(); // create an instance of FirestoreClient
+    try {
+      final Doc doc = await client.getDoc(ref); // get a document from Firestore
+      return Result.success(doc); // return a successful result
+    } catch (e) {
+      return const Result.failure(); // return a failed result
+    }
+  }
+}
