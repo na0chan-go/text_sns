ddd8886b12612c937b63a7ce66e393d03ce884ae
na0chan-go <nn19960218@gmail.com>
2024-06-04 00:43:07 +0900

    feat: createDocメソッドを作成

--- lib/repository/firestore_repository.dart
+++ lib/repository/firestore_repository.dart
@@ -4,6 +4,16 @@ import 'package:text_sns/typedefs/firestore_typedef.dart';
 import 'package:text_sns/typedefs/result_typedef.dart';
 
 class FirestoreRepository {
+  FutureResult<bool> createDoc(DocRef ref, SDMap json) async {
+    final client = FirestoreClient(); // create an instance of FirestoreClient
+    try {
+      await client.createDoc(ref, json); // create a document in Firestore
+      return const Result.success(true); // return a successful result
+    } catch (e) {
+      return const Result.failure(); // return a failed result
+    }
+  }
+
   FutureResult<Doc> getDoc(DocRef ref) async {
     final client = FirestoreClient(); // create an instance of FirestoreClient
     try {
