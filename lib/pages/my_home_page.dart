147d576330ae6e56580a7a361a9f8ee8b90cb25d
na0chan-go <nn19960218@gmail.com>
2024-06-04 00:04:57 +0900

    feat: デフォルトの値を設定

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -15,9 +15,7 @@ class MyHomePage extends StatelessWidget {
         onPressed: () async {
           try {
             const data = PublicUser(
-              followerCount: 0,
-              followingCount: 0,
-              uid: 'second',
+              uid: 'third',
             );
             final json = data.toJson();
             await FirebaseFirestore.instance
