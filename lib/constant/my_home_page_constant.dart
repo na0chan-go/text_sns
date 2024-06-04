20091d964dd05d9a2e834945de3945a6355880cb
na0chan-go <nn19960218@gmail.com>
2024-06-05 00:18:54 +0900

    feat: 定数を追加

---
+++ lib/constant/my_home_page_constant.dart
@@ -0,0 +1,5 @@
+class MyHomePageConstant {
+  static const createUserFailureMsg = 'Failed to create user';
+  static const readUserSuccessMsg = 'User read succeeded';
+  static const readUserFailureMsg = 'Failed to read user';
+}
