8c456e98e1f3d3012508fb83f4f2cceaba271dc3
na0chan-go <nn19960218@gmail.com>
2024-06-08 02:00:07 +0900

    feat: AuthScreenを作成

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -3,4 +3,7 @@ import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   final rxAuthUser = Rx<User?>(null);
+  final rxIsLoginMode = true.obs;
+  void onToggleIsLoginModeButtonPressed() => _toggleIsLoginMode();
+  void _toggleIsLoginMode() => rxIsLoginMode.value = !rxIsLoginMode.value;
 }
