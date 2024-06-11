1d2accaece59a8f14f709bbee35d6012d7f268b5
na0chan-go <nn19960218@gmail.com>
2024-06-11 23:11:41 +0900

    feat: ログイン画面を作成

---
+++ lib/view/abstract/auth_state.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:text_sns/controllers/auth_controller.dart';
+
+abstract class AuthState<T extends StatefulWidget> extends State<T> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          titleWidget(),
+          _signupForm(),
+          _positiveButton(),
+          toggleLoginModeButton(),
+        ],
+      ),
+    );
+  }
+
+  // タイトル関数
+  Widget titleWidget();
+
+  // 入力フォーム関数
+  Widget _signupForm() {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ));
+  }
+
+  // email入力をする関数
+  Widget _emailTextField() {
+    final controller = AuthController.to;
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText: 'メールアドレス',
+      ),
+      // フォームフィールドの情報を変数に保存する
+      onSaved: controller.setEmail,
+      validator: (value) {
+        // メールアドレスのバリデーション
+        return GetUtils.isEmail(value!) ? null : 'メールアドレスを入力してください';
+      },
+    );
+  }
+
+  // password入力をする関数
+  Widget _passwordTextField() {
+    final controller = AuthController.to;
+    return TextFormField(
+      obscureText: true, // パスワードを隠す
+      decoration: const InputDecoration(
+        hintText: 'パスワード',
+      ),
+      onSaved: controller.setPassword,
+      validator: (value) {
+        // パスワードのバリデーション
+        return value!.length >= 8 ? null : 'パスワードは8文字以上で入力してください';
+      },
+    );
+  }
+
+  Widget toggleLoginModeButton();
+
+  // 送信ボタン関数
+  Widget _positiveButton() {
+    return ElevatedButton(
+      onPressed: () {
+        // バリデーションを行う
+        if (_formKey.currentState!.validate()) {
+          // フォームフィールドの情報を変数に保存する
+          _formKey.currentState!.save();
+        }
+        AuthController.to.onPositiveButtonPressed();
+      },
+      child: const Text('送信'),
+    );
+  }
+}
--- lib/view/pages/my_home_page/components/auth_screen/components/login_screen.dart
+++ lib/view/pages/my_home_page/components/auth_screen/components/login_screen.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:text_sns/controllers/auth_controller.dart';
 import 'package:text_sns/view/abstract/auth_state.dart';
 
-class SignupScreen extends StatefulWidget {
-  const SignupScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<SignupScreen> createState() => _SignupScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _SignupScreenState extends AuthState<SignupScreen> {
+class _LoginScreenState extends AuthState<LoginScreen> {
   @override
   Widget titleWidget() {
     return const Text(
-      '新規登録',
+      'ログイン',
       style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
     );
   }
@@ -22,6 +22,6 @@ class _SignupScreenState extends AuthState<SignupScreen> {
   Widget toggleLoginModeButton() {
     return TextButton(
         onPressed: AuthController.to.onToggleIsLoginModeButtonPressed,
-        child: const Text('ログイン画面へ'));
+        child: const Text('新規登録画面へ'));
   }
 }
