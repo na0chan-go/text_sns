2f2a28e5572c5ab04071fddd410ec1b6c50779a5
na0chan-go <nn19960218@gmail.com>
2024-06-12 21:46:39 +0900

    feat: ユーザー名を編集する画面を作成

--- lib/view/pages/my_home_page/components/main_screen/components/content_screen.dart
+++ lib/view/pages/my_home_page/components/main_screen/components/content_screen.dart
@@ -3,17 +3,16 @@ import 'package:get/get.dart';
 import 'package:text_sns/controllers/auth_controller.dart';
 import 'package:text_sns/controllers/main_controller.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class ContentScreen extends StatelessWidget {
+  const ContentScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    final mainController = Get.put(MainController());
     final authController = AuthController.to;
     const style = TextStyle(fontSize: 30.0);
     return Column(
       children: [
         Obx(
-          () => Text(mainController.rxPublicUser.value?.uid ?? 'null',
+          () => Text(MainController.to.rxPublicUser.value?.uid ?? 'null',
               style: style),
         ),
         const SizedBox(height: 20.0),
